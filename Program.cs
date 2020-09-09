e93f5c3009d8d259d7e06f536a465412d7602bad
Ryan Oliveira013 <RyanOliveira.2003.c@gmail.com>
2020-09-09 00:12:09 -0300

    Configuraçao do status inicial do programa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Etec_2020_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OlaMundo");
+        }
+    }
+}
